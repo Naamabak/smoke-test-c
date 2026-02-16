3f311b5e07b5d559de48535516839790afd4e483
Naama <bak.naama@gmail.com>
2026-02-16 22:38:44 +0100

    docs(include/doxy_smoke/callbacks.h): add Doxygen documentation
    Generated by UnderstandDocs

--- include/doxy_smoke/callbacks.h
+++ include/doxy_smoke/callbacks.h
@@ -15,6 +15,11 @@ typedef struct ds_callbacks {
     void* user;
 } ds_callbacks;
 
+/**
+ * @brief Set the callback functions for the data stream.
+ * @param cb The callback structure to be registered.
+ * @return The status of the operation as a ds_err value.
+ */
 ds_err ds_set_callbacks(ds_callbacks cb);
 
 /** @brief Emits a log message (used internally). */
