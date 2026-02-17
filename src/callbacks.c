a8bae6b07f4d832597d35f7623f5aa91634b170a
Naama <bak.naama@gmail.com>
2026-02-17 16:42:20 +0100

    docs(src/callbacks.c): add Doxygen documentation
    Generated by UnderstandDocs

--- src/callbacks.c
+++ src/callbacks.c
@@ -2,11 +2,21 @@
 
 static ds_callbacks g_cb = {0};
 
+/**
+ * @brief Register the callback functions for the data stream.
+ * @param cb The structure containing the callback function pointers.
+ * @return A ds_err status code indicating the result of the operation.
+ */
 ds_err ds_set_callbacks(ds_callbacks cb) {
     g_cb = cb;
     return DS_OK;
 }
 
+/**
+ * @brief Dispatch a formatted message to the logging system at a specified severity level.
+ * @param level The severity level associated with the message.
+ * @param msg The null-terminated string containing the message to be logged.
+ */
 void ds_emit_log(int level, const char* msg) {
     if (g_cb.logger) {
         g_cb.logger(level, msg, g_cb.user);
